ec5e6bfd441ed800ab187251d724af902daa52f5
sanoj <sanojchandran02@gmail.com>
2026-07-18 01:05:27 +0530

    two instances were added

--- variable.tf
+++ variable.tf
@@ -10,3 +10,13 @@ variable "sub_name" {
     "2" = "pub-1"
   }
 }
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+# variable "instance_name" {
+#   description = "EC2 instance name"
+#   default     = "Provisioned by Terraform"
+# }
