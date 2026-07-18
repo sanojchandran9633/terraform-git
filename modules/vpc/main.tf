62dcf258346962f2a084cef2212942b4435e7a54
sanoj <sanojchandran02@gmail.com>
2026-07-18 20:32:05 +0530

    removed eip & NGWY2222

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -50,15 +50,15 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.this.id
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat.id
+#   }
+# }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
