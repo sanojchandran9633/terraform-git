b7bd975efc18e5d70256a10b646eb6a7b8016372
sanoj <sanojchandran02@gmail.com>
2026-07-18 17:33:19 +0530

    Tags corrected

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,5 @@
 variable "vpc_cidr" {}
 variable "public_subnet_cidr" {}
 variable "private_subnet_cidr" {}
-variable "pub_name" {
-    type = string
-    "Pub_Subnet"
-    }
-variable "pri_name" {
-    type = string
-    "Pri_Subnet"
-    }
+variable "pub_name" {"Pub_Subnet"}
+variable "pri_name" {"Pri_Subnet"}
